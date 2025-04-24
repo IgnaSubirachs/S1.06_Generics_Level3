d2860462d0bc42b87470c29bd33271e366a704d1
Ignasi Subirachs <ignasubirachs@gmail.com>
2025-04-24 11:06:53 +0200

    Update Generic.java

--- src/Generic.java
+++ src/Generic.java
@@ -3,8 +3,7 @@ public class Generic {
 
     public <T extends Phone> void phoneMethod(T phone) {
         phone.call();
-        // We cannot call takePhoto() here because T is only known as a Phone,
-        // and Phone interface does not have takePhoto()
+
 
     }
 
